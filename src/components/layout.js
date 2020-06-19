4dac420851caca3864cb5856574c5799a9dd56f4
gdstewart <prototeutonic@gmail.com>
2020-06-19 13:48:13 -0300

    Added mobile compatibility message

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,14 @@ import Navbar from "./navbar";
 import Player from "./player";
 import LoadingOverlay from "./loadingoverlay";
 import PopUp from "./popup";
+import MobileView from "./mobileview";
 import PlaybackStore from "../stores/playback";
 import AppStore from "../stores/app";
 import { observer } from "mobx-react";
 
 const Layout = observer(props => (
     <div className="layout">
+        <MobileView />
         {AppStore.loading ? <LoadingOverlay /> : null}
         {AppStore.showPopUp ? <PopUp /> : null}
         <Navbar />
---
+++ src/components/mobileview.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { FaGooglePlay } from "react-icons/fa";
+
+export default class MobileView extends Component {
+    render() {
+        return (
+            <div className="mobile-view fade-in">
+                <div className="mobile-view-logo" />
+                <div className="mobile-view-header">Sorry!</div>
+                <div className="mobile-view-blurb">orb is only available on desktop, tablets, and the Google Play store.</div>
+                <div className="mobile-view-blurb">Download here:</div>
+                <ul className="mobile-view-links">
+                    <li key={"googleplay"} className="mobile-view-link">
+                        <FaGooglePlay className="mobile-view-link-icon" />
+                        <a className="mobile-view-link-text" href={"https://play.google.com/store/apps/details?id=com.orbmobile"} target="_blank" rel="noopener noreferrer">Android version</a>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
